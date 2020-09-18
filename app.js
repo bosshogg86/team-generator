6ae62e7d88d25c22c716c1716dab142b293a79a3
bosshogg86 <jeffhogg86@gmail.com>
2020-09-17 20:20:19 -0700

    writes team.html, renders manager

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -12,7 +14,53 @@ console.log(outputPath);
 const render = require("./lib/htmlRenderer");
 
 const team = [];
+const questions = [
+  {
+    type: "input",
+    name: "managerName",
+    message: "What is the manager's name?",
+  },
+  {
+    type: "input",
+    name: "managerId",
+    message: "What is the manager's employee Id?",
+  },
+  {
+    type: "input",
+    name: "managerEmail",
+    message: "What is the manager's email address?",
+  },
+  {
+    type: "list",
+    name: "managerOfficeNumber",
+    message: "What is the manager's office number?",
+    choices: [1, 2, 3, 4],
+  },
+];
+const promptUser = () => inquirer.prompt(questions);
 
+const init = async () => {
+  try {
+    const response = await promptUser();
+
+    const manager = new Manager(
+      response.managerName,
+      response.managerId,
+      response.managerEmail,
+      response.managerOfficeNumber
+    );
+
+    team.push(manager);
+
+    writeFileAsync("team.html", render(team));
+
+    console.log("Successfully wrote to team.html");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+init();
 // const e = new Engineer("Hannah Folk", 1, "hfolk25@gmail.com", "hannahfolk");
 // team.push(e);
 // fs.writeFile("team.html", render(team), function (err) {
