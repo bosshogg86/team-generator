baca82312b4d7ef7a8fbab743ebea008268cee7a
bosshogg86 <jeffhogg86@gmail.com>
2020-09-18 14:59:23 -0700

    adds comments

--- app.js
+++ app.js
@@ -6,11 +6,8 @@ const path = require("path");
 const fs = require("fs");
 const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile);
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
-console.log(outputPath);
 const render = require("./lib/htmlRenderer");
 
 const team = [];
@@ -35,24 +32,6 @@ const managerQuestions = [
     name: "officeNumber",
     message: "What is the manager's office number?",
     choices: [1, 2, 3, 4],
-  },
-];
-
-const add = [
-  {
-    type: "list",
-    name: "addEmployee",
-    message: "Do you have another employee to enter?",
-    choices: ["yes", "no"],
-  },
-];
-
-const which = [
-  {
-    type: "list",
-    name: "employee",
-    message: "Which type of employee do you want to enter?",
-    choices: ["engineer", "intern"],
   },
 ];
 
@@ -102,30 +81,49 @@ const internQuestions = [
   },
 ];
 
+const add = [
+  {
+    type: "list",
+    name: "addEmployee",
+    message: "Do you have another employee to enter?",
+    choices: ["yes", "no"],
+  },
+];
+
+const which = [
+  {
+    type: "list",
+    name: "employee",
+    message: "Which type of employee do you want to enter?",
+    choices: ["engineer", "intern"],
+  },
+];
+
 const managerPrompt = () => inquirer.prompt(managerQuestions);
 const engineerPrompt = () => inquirer.prompt(engineerQuestions);
 const internPrompt = () => inquirer.prompt(internQuestions);
 const addPrompt = () => inquirer.prompt(add);
-
 const whichPrompt = () => inquirer.prompt(which);
 
 const init = async () => {
   try {
+    // Creates manager
     const m = await managerPrompt();
-
     const manager = new Manager(m.name, m.id, m.email, m.officeNumber);
-
     team.push(manager);
-
     let addRes = await addPrompt();
 
     while (addRes.addEmployee === "yes") {
       const which = await whichPrompt();
+
+      // Creates engineer
       if (which.employee === "engineer") {
         const e = await engineerPrompt();
         const engineer = new Engineer(e.name, e.id, e.email, e.github);
         team.push(engineer);
         addRes = await addPrompt();
+
+        // Creates intern
       } else {
         const i = await internPrompt();
         const intern = new Intern(i.name, i.id, i.email, i.school);
@@ -134,10 +132,12 @@ const init = async () => {
       }
     }
 
+    // Creates output folder if it does not exist
     if (!fs.existsSync(OUTPUT_DIR)) {
       fs.mkdirSync(OUTPUT_DIR);
     }
 
+    // Writes file
     writeFileAsync(outputPath, render(team));
 
     console.log("Successfully wrote to team.html");
@@ -147,9 +147,3 @@ const init = async () => {
 };
 
 init();
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above to target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
