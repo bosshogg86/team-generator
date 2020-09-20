c96aa507428b3a73d7ceff4aa4a66d220ce93f32
bosshogg86 <jeffhogg86@gmail.com>
2020-09-19 17:35:25 -0700

    creates questions.js

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const {
+  managerQuestions,
+  engineerQuestions,
+  internQuestions,
+  add,
+  which,
+} = require("./lib/questions");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -9,95 +16,13 @@ const writeFileAsync = util.promisify(fs.writeFile);
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
-
+const Validator = require("validatorjs");
+// let nameValidator = new Validator(m.name, rules, ["Enter a valid input"]);
+// let rules = {
+//   name: "required",
+//   email: "required|email",
+// };
 const team = [];
-const managerQuestions = [
-  {
-    type: "input",
-    name: "name",
-    message: "What is the team manager's name?",
-  },
-  {
-    type: "input",
-    name: "id",
-    message: "What is the manager's employee Id?",
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "What is the manager's email address?",
-  },
-  {
-    type: "list",
-    name: "officeNumber",
-    message: "What is the manager's office number?",
-    choices: [1, 2, 3, 4],
-  },
-];
-
-const engineerQuestions = [
-  {
-    type: "input",
-    name: "name",
-    message: "Who is the team engineer?",
-  },
-  {
-    type: "input",
-    name: "id",
-    message: "What is the engineer's employee Id?",
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "What is the engineer's email address?",
-  },
-  {
-    type: "input",
-    name: "github",
-    message: "What is the engineer's github username?",
-  },
-];
-
-const internQuestions = [
-  {
-    type: "input",
-    name: "name",
-    message: "Who is the team intern?",
-  },
-  {
-    type: "input",
-    name: "id",
-    message: "What is the intern's employee Id?",
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "What is the intern's email address?",
-  },
-  {
-    type: "input",
-    name: "school",
-    message: "What school did the intern attend?",
-  },
-];
-
-const add = [
-  {
-    type: "list",
-    name: "addEmployee",
-    message: "Do you have another employee to enter?",
-    choices: ["yes", "no"],
-  },
-];
-
-const which = [
-  {
-    type: "list",
-    name: "employee",
-    message: "Which type of employee do you want to enter?",
-    choices: ["engineer", "intern"],
-  },
-];
 
 const managerPrompt = () => inquirer.prompt(managerQuestions);
 const engineerPrompt = () => inquirer.prompt(engineerQuestions);
